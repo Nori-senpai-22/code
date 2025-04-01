1059dc5ab3e8b0003ebbcda48b2f8667ae89cf67
Nori-senpai-22 <abangifaith2020@gmail.com>
2025-04-01 12:02:48 +0200

    Create grades.py

---
+++ grades.py
@@ -0,0 +1,28 @@
+Holy moly, because the class's average was super low on the test, the teacher just added a curve for the test grades! 😭
+
+Create a grades.py program that checks whether a grade is above or below 55.
+
+Start by creating a variable called grade and give it a value between 0-100.
+
+Write a if/else statement for the following:
+
+If grade is greater than or equal to 55, then print "You passed."
+Else, print "You failed."
+
+
+
+
+
+
+
+
+
+
+
+grade = int (input("What is ypur grade?"))
+
+if grade >= 55:
+  print("You Passed!")
+else:
+  print("Failed!")
+
